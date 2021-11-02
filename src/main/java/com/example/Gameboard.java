d9e28d11e34fe8831fba8b5fae42c450f224e796
Gurra <g.linnar@gmail.com>
2021-11-02 16:43:31 +0100

    Added test for counting cells created

--- src/main/java/com/example/Gameboard.java
+++ src/main/java/com/example/Gameboard.java
@@ -1,23 +1,37 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Gameboard {
 
-    private int width;
-    private int height;
+
+    /*
+     1. Any live cell with fewer than two live neighbors dies, as if caused by
+underpopulation.
+ 2. Any live cell with more than three live neighbors dies, as if by
+overcrowding.
+ 3. Any live cell with two or three live neighbors lives on to the next
+generation.
+ 4. Any dead cell with exactly three live neighbors becomes a live cell.
+    *
+    *
+    * */
+    private int boardWidth;
+    private int boardHeight;
     int[][] board;
 
     public Gameboard(int width, int height) {
-        this.width = width;
-        this.height = height;
+        this.boardWidth = width;
+        this.boardHeight = height;
         this.board = new int[width][height];
     }
 
-    public int getWidth() {
-        return width;
+    public int getBoardWidth() {
+        return boardWidth;
     }
 
-    public int getHeight() {
-        return height;
+    public int getBoardHeight() {
+        return boardHeight;
     }
 
     public int[][] getBoard() {
@@ -25,14 +39,14 @@ public class Gameboard {
     }
 
 
-    public void printGameBoard(){
+    public void printGameBoard() {
         System.out.println("---");
-        for (int y = 0; y < height; y++) {
+        for (int y = 0; y < boardHeight; y++) {
             String line = "|";
-            for(int x = 0; x<height; x++){
-                if(this.board[x][y] == 0){
+            for (int x = 0; x < boardHeight; x++) {
+                if (this.board[x][y] == 0) {
                     line += ".";
-                }else {
+                } else {
                     line += "*";
                 }
             }
@@ -41,6 +55,12 @@ public class Gameboard {
 
         }
         System.out.println("---\n");
+    }
+
+    public void addCells(int x , int y){
+        this.board[x][y] = 1;
+
+
     }
 
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,13 +1,17 @@
 import com.example.Gameboard;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 
 
 class MainTest {
 
-    Gameboard gameboard = new Gameboard(20,20);
+    Gameboard gameboard = new Gameboard(10,10);
     @Test
     void makeSureGameBoardObjectIsNotEmpty(){
 
@@ -19,6 +23,35 @@ class MainTest {
 
         gameboard.printGameBoard();
 
+        assertEquals(10,gameboard.getBoardWidth());
+        assertEquals(10,gameboard.getBoardHeight());
+
+
+    }
+    @Test
+    void addLivingCellsToGameBoard(){
+        gameboard.addCells(0,0);
+        gameboard.addCells(1,6);
+        gameboard.addCells(2,5);
+        gameboard.addCells(3,8);
+        gameboard.addCells(4,9);
+        gameboard.addCells(5,1);
+        gameboard.addCells(6,2);
+
+        //assertNotNull(gameboard.getBoard());
+        gameboard.printGameBoard();
+
+        String count = Arrays.deepToString(gameboard.getBoard());
+        System.out.println(count);
+
+        assertThat(gameboard.getBoard()).isNotEmpty();
+
+       // assertThat(cellCount).isEqualTo(0);
+    }
+
+    @Test
+    void checkAmountofAliveCellsMatchesCellsCreated(){
+        assertEquals(7 , gameboard.getLivingCellsAmount());
     }
 
 }
